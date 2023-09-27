8b0aceae68bfd071f0f5ad6003508c6fd2581536
mansiks01 <mansiks321@gmail.com>
2023-09-27 22:54:41 +0530

    27/9/2023

--- BookStrore/bookstore/settings.py
+++ BookStrore/bookstore/settings.py
@@ -135,8 +135,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'bookstrore.mansi@gmail.com'
+EMAIL_HOST_PASSWORD = 'ugyrtkgxubhssstp'
 
 
 LOGIN_URL = '/'
