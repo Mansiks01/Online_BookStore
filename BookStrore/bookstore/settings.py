c611b5b0a6fbf843e2db4a938f0796a9e2c67bab
mansiks01 <mansiks321@gmail.com>
2023-09-29 12:18:02 +0530

    out_of_stock and filter

--- BookStrore/bookstore/settings.py
+++ BookStrore/bookstore/settings.py
@@ -138,8 +138,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bookstrore.mansi@gmail.com'
-EMAIL_HOST_PASSWORD = 'ugyrtkgxubhssstp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # AUTH_USER_MODEL = 'models.CustomUser'
 
